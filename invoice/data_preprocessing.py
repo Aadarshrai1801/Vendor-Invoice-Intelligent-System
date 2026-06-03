a670a7f25319c4b01fdfbf957b561ba0fbeebebc
Aadarshrai1801 <aadarshrai1801@gmail.com>
2026-06-03 22:03:28 +0530

    Third commit

---
+++ invoice/data_preprocessing.py
@@ -0,0 +1,20 @@
+import sqlite3
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+def load_invoice_data():
+    conn = sqlite3.connect("C:/Users/Aadarsh/Desktop/Vendor Invoice Intelligent System/data/inventory.db")
+    
+    query = """
+    WITH purchase_agg AS (
+        SELECT
+        p.pONumber,
+        COUNT(DISTINCT p.Brand) AS total_brands,
+        SUM(p.Quantity) AS total_item_quantity,
+        SUM(p.Dollars) AS total_items_dollars,
+        AVG(julianday(p.ReceivingDate))
+    )
+    """
+    
